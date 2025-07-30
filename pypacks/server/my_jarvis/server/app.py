b1dfd01959d7ab98e820549d5f48cdcd25544029
Rahul Kandekar <rahukand@gmail.com>
2025-07-29 22:19:42 -0700

    🚀 deployment commit

--- pypacks/server/my_jarvis/server/app.py
+++ pypacks/server/my_jarvis/server/app.py
@@ -17,39 +17,39 @@ GOOGLE_CLIENT_SECRET=get_var("GOOGLE_CLIENT_SECRET")
 GOOGLE_REDIRECT_URI=get_var("GOOGLE_REDIRECT_URI")
 
 app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.get("/login/google2")
-def login_google():
-    url = (
-        f"https://accounts.google.com/o/oauth2/auth"
-        f"?response_type=code"
-        f"&client_id={GOOGLE_CLIENT_ID}"
-        f"&redirect_uri={GOOGLE_REDIRECT_URI}"
-        f"&scope=openid%20profile%20email"
-        f"&access_type=offline"
-    )
-    # return RedirectResponse(url)
-    return "Hello"
+# @app.get("/login/google2")
+# def login_google():
+#     url = (
+#         f"https://accounts.google.com/o/oauth2/auth"
+#         f"?response_type=code"
+#         f"&client_id={GOOGLE_CLIENT_ID}"
+#         f"&redirect_uri={GOOGLE_REDIRECT_URI}"
+#         f"&scope=openid%20profile%20email"
+#         f"&access_type=offline"
+#     )
+#     # return RedirectResponse(url)
+#     return "Hello"
 
-@app.get("/auth/callback")
-def auth_google(code: str):
-    token_url = "https://accounts.google.com/o/oauth2/token"
-    data = {
-        "code": code,
-        "client_id": GOOGLE_CLIENT_ID,
-        "client_secret": GOOGLE_CLIENT_SECRET,
-        "redirect_uri": GOOGLE_REDIRECT_URI,
-        "grant_type": "authorization_code",
-    }
-    response = requests.post(token_url, data=data)
-    access_token = response.json().get("access_token")
-    user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
-    return user_info.json()
+# @app.get("/auth/callback")
+# def auth_google(code: str):
+#     token_url = "https://accounts.google.com/o/oauth2/token"
+#     data = {
+#         "code": code,
+#         "client_id": GOOGLE_CLIENT_ID,
+#         "client_secret": GOOGLE_CLIENT_SECRET,
+#         "redirect_uri": GOOGLE_REDIRECT_URI,
+#         "grant_type": "authorization_code",
+#     }
+#     response = requests.post(token_url, data=data)
+#     access_token = response.json().get("access_token")
+#     user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
+#     return user_info.json()
 
-@app.get("/token")
-async def get_token(token: str = Depends(oauth2_scheme)):
-    return jwt.decode(token, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
+# @app.get("/token")
+# async def get_token(token: str = Depends(oauth2_scheme)):
+#     return jwt.decode(token, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
 
 
 
